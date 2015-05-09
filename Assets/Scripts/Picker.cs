95857c7f732e4823d04c961e812383ec72fd4b87
Sachit Vithaldas <macintoshsac@gmail.com>
2015-05-09 11:06:30 -0700

    Added 2 new Mechanics
    You can also pickup swag when you go to the boxes.
    Throw Swag using the mouse click
    You can pick up the guitar on the side and move it around

---
+++ Assets/Scripts/Picker.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Picker : MonoBehaviour {
+
+	// Use this for initialization
+	public GameObject camera;
+
+	private GameObject carriedObject;
+
+	bool carrying;
+
+	void Start()
+	{
+		carrying = false;
+		carriedObject = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!carrying)
+		{
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				Ray r = camera.camera.ScreenPointToRay(new Vector2(Screen.width/2, Screen.height/2));
+				RaycastHit hit;
+
+				if(Physics.Raycast(r, out hit))
+				{
+					if(hit.distance < 2)
+					{
+						Pickupable p = hit.collider.gameObject.GetComponent<Pickupable>();
+
+						if(p)
+						{
+							carrying = true;
+							carriedObject = hit.collider.gameObject;
+						}
+					}
+				}
+			}
+		}
+
+		else
+		{
+			if(Input.GetKeyDown(KeyCode.E))
+			{
+				carriedObject = null;
+				carrying = false;
+			}
+			if(carriedObject)
+			{
+				carriedObject.transform.position = Vector3.Lerp(carriedObject.transform.position, transform.position + transform.forward*2, Time.deltaTime*60);
+			}
+		}
+	
+	}
+}
--- Readme.md
+++ Readme.md
@@ -4,9 +4,15 @@ When the player comes up on stage, the crowd claps to welcome him
 They start dancing when he starts playing the music
 When he finishes playing the music they start clapping.
 When he leaves, they go back to idling.
+You can also pickup swag when you go to the boxes.
+Throw Swag using the mouse click
+You can pick up the guitar on the side and move it around
 
 INSTRUCTIONS
 1. Use Unity to open the Stadium Scene inside /Assets/Scenes
 2. Press the play button
 3. Use the WASD keys to navigate, mouse to look around. Use space to jump.
 4. Go near instruments and press P when prompted to toggle them on or off
+5. Go the the boxes to pickup swag and use the mouse click to throw it.
+6. Go to the guitar on the right side and press E to hold it.
+7. Press E to release the guitar.
